a2f7a1f31c7fe36d2413232840ec04043ac5b44b
jvhuebner <jvhuebner@ucsd.edu>
2024-05-07 22:08:24 -0700

    completed explore and expose

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,57 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const voiceDrop = document.getElementById("voice-select");
+
 function init() {
   // TODO
+  const winSpeach = window.speechSynthesis;
+  let voiceList =[];
+
+  //populate the list of voices mozilla documentation
+  //modified from 
+  function populateDropDown() {
+    voiceList =winSpeach.getVoices();
+
+    for (let i = 0; i < voiceList.length; i++ ) {
+      const curr = document.createElement("option" );
+      curr.setAttribute( "data-name", voiceList[i].name);
+      curr.setAttribute("data-lang", voiceList[i].lang);
+      curr.textContent = `${voiceList[i].name}` +" " +  `(${voiceList[i].lang})`;
+      voiceDrop.appendChild(curr);
+    }
+  }
+
+  //call function to populate drowdown of voices
+  //modified from mozilla documentation
+  populateDropDown();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateDropDown;
+  }
+
+  //say text in text box on click
+  const inField = document.querySelector("textarea");
+  let inTex ="";
+  const play = document.querySelector("button");
+  inField.addEventListener("change", ()=>{ inTex =inField.value })
+  const img = document.querySelector("img");
+
+  play.addEventListener("click", ()=>{
+    const words = new SpeechSynthesisUtterance(inTex);
+    words.voice =  voiceList[voiceDrop.selectedIndex -1];
+    winSpeach.speak(words);
+
+    img.setAttribute("src", "assets/images/smiling-open.png");
+
+    const spInt = setInterval(() => {
+      if (!winSpeach.speaking) {
+
+          clearInterval(spInt);
+          img.setAttribute("src", "assets/images/smiling.png");
+      }
+    }, 100);
+
+  })
+
+
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,58 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  
+  const aud =document.querySelector("audio");
+  const hornSel = document.querySelector("#horn-select");
+  const hornImg =document.querySelector("#expose img");
+  
+  // take input on which horn to select audio file and image
+  hornSel.addEventListener("input", function () {
+    aud.setAttribute("src", "assets/audio/" + hornSel.value + ".mp3");
+    hornImg.setAttribute("src", "assets/images/" + hornSel.value + ".svg");
+    
+  });
+
+  //take input on volume level to select volImg and volume level
+  const volSel = document.querySelector("#volume");
+  const volImg = document.querySelector("#volume-controls img");
+
+  volSel.addEventListener("input", function () {
+    //set volume level of audio
+    let vol = parseInt(volSel.value);
+    aud.volume = vol / 100;
+
+    //set volLevel for icon
+    let volLev = 0;
+    if (vol == 0){
+      volLev = 0;
+    } else if (vol <33 ) {
+      volLev = 1;
+    } else if (vol < 67) {
+      volLev = 2;
+    } else if (vol > 66){
+      volLev = 3;
+    }
+    //select icon based on volLev
+    volImg.setAttribute("src", "assets/icons/volume-level-" + volLev + ".svg");
+
+  });
+
+  //execute sound and confetti on click
+  const con = new JSConfetti();
+  const audioButton = document.querySelector("button");
+  audioButton.addEventListener("click", function () {
+    if (aud.getAttribute("src") == "") { 
+      //dont do anything if no audio selected
+    } else {
+
+      if (hornSel.value == "party-horn") {
+        con.addConfetti();
+      }
+
+      aud.play();
+    }
+  });
+  
+
 }
